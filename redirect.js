657a5068c323c7eba61a4b320924c074991a5f43
Wiyns <189463691+Wiyns@users.noreply.github.com>
2024-11-29 05:56:32 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ redirect.js
@@ -0,0 +1,22 @@
+console.log("I am literally a Omnisexual Femboy :3");
+
+function ytr() {
+   window.location.href = "https://youtube.com/@wiyns";
+}
+function ttp() {
+   window.location.href = "https://www.tiktok.com/@wiyns_";
+}
+function propag() {
+   window.location.href = "https://en.pronouns.page/@wiyns_";
+}
+function spot() {
+   window.location.href = "https://open.spotify.com/user/31u4n4jowijar2p7esr7tnkdpbmy?si=Rcyiq_uWTYmY7UP_e_3ZzA";
+}
+function git() {
+   window.location.href = "https://github.com/wiyns"
+}
+/*
+function abbr() {
+   window.location.href = "";
+}
+*/
